6e4672ce754fe6c13df4f83e5fab9d155d016721
bai <15901211173@163.com>
2020-08-10 10:37:28 +0800

    upload by bzc on 2020.8.10

---
+++ draw.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import os
+
+
+def main():
+    gtpath='/home/bai/bzc/paint/gt'
+    impath='/home/bai/bzc/paint/img'
+    simpath='/home/bai/bzc/paint/simg'
+    gtfiles=os.listdir(gtpath)
+    imfiles=os.listdir(impath)
+
+    for imfile in imfiles:
+    	imfilename=os.path.join(impath,imfile)
+    	img = cv2.imread(imfilename)
+    	with open(os.path.join(gtpath, imfile[:-4] + ".txt"), 'r', encoding='utf-8') as f:
+    		for line in f.readlines():
+    			line = [int(i) for i in line.strip().split(',')]
+    			new_img = cv2.rectangle(img, tuple(line[0:2]), tuple(line[4:6]), (0, 255, 0))
+    			simfilename=os.path.join(simpath,imfile)
+    			cv2.imwrite(simfilename, new_img)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,9 @@
+# A project for drawwing the bounding box according to the corrdinates
+ 
+Enviroment:python3,opencv-python 
+
+"gtpath"  is the folder for gt file(up-left、up-right、bottom-right and bottom-left corrdinates of bbox),
+"impath"  is the folder for the pictures before drawwing,
+"simpath" is the folder for the pictures after drawwing
+
+run with "python3 draw.py"
